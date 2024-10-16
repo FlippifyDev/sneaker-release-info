72f0b03c6cf0349500c41ff6b7df676a9a2a4b06
RoyalGr4pe <royal.melon.dev@gmail.com>
2024-10-16 19:06:18 +0100

    initial commit for working sneaker release info

---
+++ src/database.py
@@ -0,0 +1,98 @@
+from dotenv import load_dotenv
+from datetime import datetime, timezone
+import urllib.parse
+from pymongo import UpdateOne
+
+import logging
+import pymongo
+import os
+
+logger = logging.getLogger("PING-MANAGER")
+
+load_dotenv()
+
+
+class Database():
+    def __init__(self) -> None:
+        # Config
+        db_deployment =  os.getenv("DB_DEPLOYMENT")
+        db_name =        os.getenv("DB_NAME")
+        username =       os.getenv("DB_USERNAME")
+        password =       os.getenv("DB_PASSWORD")
+
+        # Config - Collections Names
+        results_col =              os.getenv("COL_RESULTS")
+
+        # Connection
+        conn_string = f"mongodb+srv://{username}:{password}@{db_deployment}.mongodb.net/"
+        self.client = pymongo.MongoClient(conn_string)
+        self.db = self.client[db_name]
+
+        # Collections
+        self.results_col = self.db[results_col]
+
+        # Create or modify the collection with changeStreamPreAndPostImages enabled
+        try:
+            self.db.create_collection(results_col, changeStreamPreAndPostImages={"enabled": True})
+        except pymongo.errors.CollectionInvalid:
+            # Collection already exists, so modify it to enable pre- and post-images
+            self.db.command({
+                'collMod': results_col,
+                'changeStreamPreAndPostImages': {'enabled': True}
+            })
+
+
+
+    def fetch_docs(self, query, projection):
+        return list(self.results_col.find(query, projection))
+    
+
+
+    def remove_all_docs(self):
+        try:
+            result = self.results_col.delete_many({})
+            logger.info(f"Deleted {result.deleted_count} documents from the collection.")
+        except Exception as e:
+            logger.error(f"An error occurred while deleting documents: {e}")
+
+
+
+    def update_products(self, products):
+        operations = []  # A list to hold the batch of updates
+
+        for product in products:
+            # The unique identifier for each product, assuming it's the link or product_name
+            query = {"link": product["link"]}
+
+            # Define the fields to update. Here we're using the `$set` operator
+            update_data = {
+                "$set": product
+            }
+
+            # Add the update operation to the batch
+            operations.append(UpdateOne(query, update_data, upsert=True))
+
+        if operations:
+            try:
+                # Perform the bulk update
+                result = self.results_col.bulk_write(operations)
+                logger.info(f"Updated {result.matched_count} products, inserted {result.upserted_count} new products.")
+            except Exception as e:
+                logger.error(f"Error updating products: {e}")
+    
+    
+
+    def add_products(self, products_to_add): 
+        try:
+            for product in products_to_add:
+                # Check if a product with the same 'deal-link' already exists
+                if self.results_col.find_one({"link": product['link']}):
+                    continue
+
+                # Insert product if it's not a duplicate
+                self.results_col.insert_one(product)
+
+            return True
+        except pymongo.errors.PyMongoError as e:
+            logger.error(f"Error while adding products to the database: {e}")
+            return False
---
+++ src/logger_setup.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+
+
+
+def delete_previous_logs_on_start(filename):
+    with open(f"logs/{filename}.log", "r+") as file:
+        file.seek(0)
+        file.truncate()
+        
+
+def setup_logger(name, filename):
+    # Logger set up
+    FORMAT = f"[{name}] | [%(asctime)s] | %(filename)s/%(funcName)s:%(lineno)d | [%(levelname)s] | %(message)s"
+    formatter = logging.Formatter(FORMAT)
+
+    # Create a FileHandler to log messages to a file
+    file_handler = logging.FileHandler(f'logs/{filename}.log', encoding='utf-8')
+    file_handler.setFormatter(formatter)
+
+    # Create a StreamHandler to log messages to the console
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
+    # Get the logger instance
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
